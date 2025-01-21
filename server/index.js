f71a6bc00aa8d4fdbb2971b3cf1f107fa80c58eb
kjainharsh <kjainharsh@gmail.com>
2025-01-22 01:45:29 +0530

    Add contact form and store data in backend

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const express = require("express");
 const app = express();
 const authRoute = require("./router/auth-router.js");
 const courseRoute = require("./router/course-router.js");
+const contactRoute = require("./router/contact-router.js");
 const connectdb = require("./utils/db.js");
 const errorMiddleware = require("./middleware/error-middleware.js");
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/data", courseRoute);
+app.use("/api/form", contactRoute);
 app.use(errorMiddleware);
 
 
