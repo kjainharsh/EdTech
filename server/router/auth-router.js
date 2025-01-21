ab8ea2ba8682017970e6dcd869afe93fb38b94bf
kjainharsh <kjainharsh@gmail.com>
2025-01-22 01:55:47 +0530

    Done ZOD validation in register and login page

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const authcontrollers = require("../controllers/auth-controller"); // Middleware for token verification
+const authcontrollers = require("../controllers/auth-controller"); 
+const validate = require("../middleware/validate-middleware");
+const { signUpSchema, loginSchema } = require("../validators/auth-validators");
 
 router.route("/").get(authcontrollers.home);
-router.route("/register").post(authcontrollers.register);
-router.route("/login").post(authcontrollers.login);
+router.route("/register").post(validate(signUpSchema),authcontrollers.register);
+router.route("/login").post(validate(loginSchema) ,authcontrollers.login);
 // router.route("/user").get(authcontrollers.user);
 
 module.exports = router;
---
+++ server/validators/auth-validators.js
@@ -0,0 +1,40 @@
+const { z } = require("zod");
+
+const loginSchema = z.object({
+    email: z
+        .string({ required_error: "Email is Required" })
+        .trim()
+        .email({ message: "Invalid Email Address" })
+        .min(3, { message: "Email must be at least 3 characters" })
+        .max(255, { message: "Email must be no more than 255 characters" }),
+    password: z
+        .string({ required_error: "Password is Required" })
+        .min(6, { message: "Password must be at least 6 characters" })
+        .max(1024, { message: "Password must be no more than 1024 characters" }),
+});
+
+//const signUpSchema = z.object({
+const signUpSchema = loginSchema.extend({
+    username: z
+        .string({ required_error: "Name is Required" })
+        .trim()
+        .min(3, { message: "Name must be at least 3 characters" })
+        .max(255, { message: "Name must be no more than 255 characters" }),
+    email: z
+        .string({ required_error: "Email is Required" })
+        .trim()
+        .email({ message: "Invalid Email Address" })
+        .min(3, { message: "Email must be at least 3 characters" })
+        .max(255, { message: "Email must be no more than 255 characters" }),
+    phone: z
+        .string({ required_error: "Phone is Required" })
+        .trim()
+        .min(10, { message: "Phone must be at least 10 characters" })
+        .max(20, { message: "Phone must be no more than 20 characters" }),
+    password: z
+        .string({ required_error: "Password is Required" })
+        .min(6, { message: "Password must be at least 6 characters" })
+        .max(1024, { message: "Password must be no more than 1024 characters" }),
+});
+
+module.exports = { signUpSchema, loginSchema };
