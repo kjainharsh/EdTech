fbbd3b566ff1b731c92fb658bf2f1dab1eecd78e
kjainharsh <kjainharsh@gmail.com>
2025-01-23 22:57:31 +0530

    Done authentication with JWT

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const authcontrollers = require("../controllers/auth-controller"); 
 const validate = require("../middleware/validate-middleware");
 const { signUpSchema, loginSchema } = require("../validators/auth-validators");
+const authMiddleware = require("../middleware/auth-middleware");
 
 router.route("/").get(authcontrollers.home);
 router.route("/register").post(validate(signUpSchema),authcontrollers.register);
 router.route("/login").post(validate(loginSchema) ,authcontrollers.login);
-router.route("/user").get(authcontrollers.user);
+router.route("/user").get(authMiddleware,authcontrollers.user);
 
 module.exports = router;
