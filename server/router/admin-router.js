2300aaafb73994ec8f7ed63985a39f31cea5d71b
kjainharsh <kjainharsh@gmail.com>
2025-01-24 00:20:32 +0530

    Authenticate admin for opening admin page

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const adminController = require("../controllers/admin-controller");
-// const authMiddleware = require("../middlewares/auth-middleware");
-// const adminMiddleware = require("../middlewares/admin-middleware");
+const authMiddleware = require("../middleware/auth-middleware");
+const adminMiddleware = require("../middleware/admin-middleware");
 const router = express.Router();
 
-router.route('/users').get(  adminController.getAllUsers);
-router.route('/contacts').get(  adminController.getAllContact);
-router.route('/users/delete/:id').delete(  adminController.deleteUserByID);
-router.route('/contacts/delete/:id').delete(  adminController.deleteContactByID);
-router.route('/users/:id').get(  adminController.getUserByID);
-router.route('/users/update/:id').patch(  adminController.updateUserById);
+router.route('/users').get(authMiddleware,adminMiddleware,  adminController.getAllUsers);
+router.route('/contacts').get(authMiddleware, adminMiddleware, adminController.getAllContact);
+router.route('/users/delete/:id').delete(authMiddleware, adminMiddleware,  adminController.deleteUserByID);
+router.route('/contacts/delete/:id').delete(authMiddleware, adminMiddleware,  adminController.deleteContactByID);
+router.route('/users/:id').get(authMiddleware, adminMiddleware, adminController.getUserByID);
+router.route('/users/update/:id').patch(authMiddleware, adminMiddleware, adminController.updateUserById);
 
 module.exports = router;
