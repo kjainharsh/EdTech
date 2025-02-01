aa48c7346c7f219e681dcf692f4483b8c9432387
kjainharsh <kjainharsh@gmail.com>
2025-02-01 14:38:37 +0530

    adding npm

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const authRoute = require("./router/auth-router.js");
 const courseRoute = require("./router/course-router.js");
 const contactRoute = require("./router/contact-router.js");
 const adminRoute = require("./router/admin-router.js");
 const connectdb = require("./utils/db.js");
 const errorMiddleware = require("./middleware/error-middleware.js");
+
+const corsOptions = {
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PATCH,DELETE,PUT,HEAD",
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
